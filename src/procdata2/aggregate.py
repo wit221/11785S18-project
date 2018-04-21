bcb6647e2e680e9a83e60d9941980c325097ac4a
wit221 <witold.dlc@gmail.com>
2018-04-20 23:42:08 -0400

    aggregate diet v1 complete

--- src/procdata2/aggregate.py
+++ src/procdata2/aggregate.py
@@ -1,12 +1,12 @@
-"""
-Script that aggregates all NHANES dietary data files into one.
-Each file has its own processing function,
-as no realistically feasible and flexible generalization is possible.
-Notes:
--discretizes continuous data: 1st iteration simplification
--discards WTDR1D, WTDR2D: weights are taken from exam. data
--discards exam-specific logistical features: examiner ID, etc.
-"""
+# """
+# Script that aggregates all NHANES dietary data files into one.
+# Each file has its own processing function,
+# as no realistically feasible and flexible generalization is possible.
+# Notes:
+# -discretizes continuous data: 1st iteration simplification
+# -discards WTDR1D, WTDR2D: weights are taken from exam. data
+# -discards exam-specific logistical features: examiner ID, etc.
+# """
 import os
 import os.path
 import itertools
@@ -39,22 +39,21 @@ def dropBetween(df, col_label_start, col_label_end):
     col_labels = list(df)
     col_labels_to_drop = col_labels[col_labels.index(col_label_start):col_labels.index(col_label_end)]
     return df.drop(col_labels_to_drop, axis=1)
-#drop all columns apart from ones in keep_cols
 def dropAllBut(df, keep_cols):
     return df.drop(df.columns.difference(keep_cols), 1)
-#fill missing sequence number rows with nans
 def fillMissingSeqnAndDropSeqn(df):
     df =  df.set_index("SEQN").reindex(pd.Index(np.arange(seqn_start,seqn_end), name="SEQN")).reset_index()
-    df = df.drop('SEQN')
+    df = df.drop('SEQN',1)
     return df
+
 def process_DRIFF_H(day_num):
-    """
-    DR*IFF_H is an ind_files
-    note: drops DR1CCMNM DR1CCMTX DR1_020 DR1_030Z DR1FS DR1_040Z
-    #TODO: include time of day, quantize into ~3-5 cats
-    End cols:
-    food1, food2, food3
-    """
+    # """
+    # DR*IFF_H is an ind_files
+    # note: drops DR1CCMNM DR1CCMTX DR1_020 DR1_030Z DR1FS DR1_040Z
+    # #TODO: include time of day, quantize into ~3-5 cats
+    # End cols:
+    # food1, food2, food3
+    # """
     #setup some var names based on day
     DRIFF_H = 'DR{}IFF_H'.format(day_num)
     DRIFDCD = 'DR{}IFDCD'.format(day_num)
@@ -70,17 +69,20 @@ def process_DRIFF_H(day_num):
     with open(file_path, 'rb') as f:
         dfm = xport.to_dataframe(f)
 
+    df=dfm
+    df['SEQN'] = dfm['SEQN'].astype(int)
+    df[DRIFDCD] = dfm[DRIFDCD].astype(int)
+
     #drop redundant data
-    df = dropAllBut(dfm, ['SEQN', 'DR1IFDCD', 'DR1IGRMS'])
+    df = dropAllBut(dfm, ['SEQN', DRIFDCD])
 
     #make core out of just seqn
     core = df['SEQN'].drop_duplicates(keep='first').to_frame()
     num_seqs = core.shape[0]
 
     # determine food encodings
-    food_codes = np.sort(df.DR1IFDCD.unique()).astype(np.int)
-    f2i = {food_codes[i]:i for i in range(len(food_codes))}
-    fc_label_format = 'DR1IFDCD-{}'
+    food_codes = np.sort(df[DRIFDCD].unique()).astype(np.int)
+    fc_label_format = DRIFDCD+'-{}'
     fc_cols = [fc_label_format.format(fc) for fc in food_codes]
     num_fc = food_codes.size
 
@@ -94,7 +96,7 @@ def process_DRIFF_H(day_num):
         dfs = df.loc[df['SEQN'] == seqn]
         for i in dfs.index:
             #set appropriate value in core.o..
-            fc = dfs.at[i, 'DR1IFDCD']
+            fc = dfs.at[i, DRIFDCD]
             core.loc[core.index[core['SEQN']==seqn], fc_label_format.format(fc)] = 1
 
     #final readifiction
@@ -113,7 +115,7 @@ def process_DRTOT_H(day_num):
     with open(file_path, 'rb') as f:
         dfm = xport.to_dataframe(f)
     df = dfm
-    df = df.dropBetween(df [WTDRD, DRTKCAL])
+    df = dropBetween(df, WTDRD, DRTKCAL)
     df = fillMissingSeqnAndDropSeqn(df)
 
     return df
@@ -163,7 +165,6 @@ def process_DSIDS_H(day_num):
             #set appropriate value in core.o..
             sc = dfs.at[i, 'DSDSUPID']
             core.loc[core.index[core['SEQN']==seqn], sc_label_format.format(day_num, sc)] = 1
-
     core = fillMissingSeqnAndDropSeqn(core)
 
     return core
@@ -223,7 +224,7 @@ def process_DSQIDS_H():
         for i in dfs.index:
             #set appropriate value in core.o..
             sc = dfs.at[i, 'DSDSUPID']
-            core.loc[core.index[core['SEQN']==seqn], sc_label_format.format(day_num, sc)] = 1
+            core.loc[core.index[core['SEQN']==seqn], sc_label_format.format(sc)] = 1
 
     core = fillMissingSeqnAndDropSeqn(core)
 
@@ -231,41 +232,55 @@ def process_DSQIDS_H():
 
 def process_DSQTOT_H():
     #setup some var names based on day
-    DSQTOT_H = 'DSQTOT_H'.format(day_num)
+    DSQTOT_H = 'DSQTOT_H'
 
     fn = DSQTOT_H
     file_path = os.path.join(data_dir, "{}.XPT".format(fn))
     with open(file_path, 'rb') as f:
         dfm = xport.to_dataframe(f)
     df = dfm
+        #convert columns of interest to int
+    df=dfm
+    df['SEQN'] = df['SEQN'].astype(int)
+
     df = dropBetween(df, 'DSDCOUNT', 'DSQTKCAL')
     df = fillMissingSeqnAndDropSeqn(df)
 
+    return df
 def aggregate():
-    #DR2IFF_H subset of DR1IFF_H subset of DR1TOT_H eq. set of DR2TOT_H
     DR1IFF_H = process_DRIFF_H(1)
     DR2IFF_H = process_DRIFF_H(2)
     DR1TOT = process_DRTOT_H(1)
     DR2TOT = process_DRTOT_H(2)
+
+    print('DR: done')
 
     DS1IDS_H = process_DSIDS_H(1)
     DS2IDS_H = process_DSIDS_H(2)
     DS1TOT_H = process_DSTOT_H(1)
     DS2TOT_H = process_DSTOT_H(2)
 
+    print('DS: done')
+
     DSQIDS_H = process_DSQIDS_H()
     DSQTOT_H = process_DSQTOT_H()
+
+    print('DSQ: done')
 
     files = [DR1IFF_H, DR2IFF_H, DR1TOT, DR2TOT, DS1IDS_H, DS2IDS_H, DS1TOT_H,
     DS2TOT_H, DSQIDS_H, DSQTOT_H]
 
     x = np.hstack([file.as_matrix() for file in files])
-    labels = list(itertools.chain.from_iterable([file.as_matrix() for file in files]))
-
-    return x, labels
+    labels = np.array(list(itertools.chain.from_iterable(files)))
+    miss_mask = np.isnan(x)
+    x[miss_mask] = 0
+    seqn = np.arange(seqn_start, seqn_end)
+    return x, labels, miss_mask, seqn
 
 if __name__ == '__main__':
-    x, labels = aggregate()
+    x, labels, miss_mask, seqn = aggregate()
     out_path = os.path.abspath('../../data')
     np.save(os.path.join(out_path, 'dietary_data.npy'), x)
     np.save(os.path.join(out_path, 'dietary_labels.npy'), labels)
+    np.save(os.path.join(out_path, 'dietary_miss_mask.npy'), miss_mask)
+    np.save(os.path.join(out_path, 'dietary_seqn.npy'), seqn)
